b50be7c185d464474376c5e27bf82149278253bc
chitrakala <chitrakala2410@gmail.com>
2026-06-30 16:25:41 +0530

    feat: initialize project structure with Gallery page and Tailwind CSS styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import About from './pages/About';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Gallery from './pages/Gallery'
 
-function App(): React.JSX.Element {
+function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-[#fcfbfe]">
-        {/* Floating sticky Navbar */}
-        <Navbar />
-
-        {/* Main Content Area */}
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<About />} />
-            {/* Fallback route back to About page */}
-            <Route path="*" element={<About />} />
-          </Routes>
-        </div>
-
-        {/* Footer */}
-        <Footer />
-      </div>
+      <Gallery />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
